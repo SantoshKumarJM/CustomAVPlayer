97aa6b4a4275c3318165a83f61820abdf45cfd1f
santosh kumar <santosh@orchids.edu.in>
2020-07-13 17:49:21 +0530

    Initial Commit

---
+++ customAVPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  customAVPlayer
+//
+//  Created by Santosh on 13/07/20.
+//  Copyright © 2020 Santosh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
